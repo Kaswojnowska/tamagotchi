4559648cfb47ee8b9faeaaa935accdb06162286c
Katarzyna Wojnowska <katarzyna_wojnowska@vilogdansk.pl>
2017-02-06 23:09:24 +0100

    moje zmiany

--- Andrzej.py
+++ Andrzej.py
@@ -12,10 +12,10 @@ class Tamagotchi:
 
 
     def powitanie(self):
-        print('Cześć, mam na imię', self.imie)
+        print('Czesc, mam na imie', self.imie)
 
     def jaka_plec(self):
-        print('Moja płeć to:', self.plec)
+        print('Moja plec to:', self.plec)
 
     def wybierz_zwierzatko(self):
         print('Jestem', self.zwierze)
@@ -26,15 +26,15 @@ class Tamagotchi:
 
     def mycie(self, kapanie):
         self.czystosc += 25
-        print('Myję się', kapanie)
+        print('Myje sie', kapanie)
 
     def leczenie(self, choroba):
         self.zdrowie += 25
-        print('Jestem chory, potrzebuję', choroba)
+        print('Jestem chory, potrzebuje', choroba)
 
     def zabawa(self, gra):
         self.szczescie += 25
-        print('Bawię się', gra)
+        print('Bawie sie', gra)
 
     def glaskanie(self):
         print('Moew moew')
@@ -48,7 +48,7 @@ class Tamagotchi:
         if self.najedzenie < 25:
             print('glodny')
         elif self.czystosc < 40:
-            print('Tamagotchi brudny - wykąp %s' % (imie))
+            print('Tamagotchi brudny - wykap %s' % (imie))
         elif self.szczescie < 20:
             print('%s jest smutny' % (imie))
         elif self.zdrowie < 10:
@@ -57,22 +57,22 @@ class Tamagotchi:
 
 
 if __name__ == '__main__':
-    print('Witaj w świecie Tamagotchi')
+    print('Witaj w swiecie Tamagotchi')
     imie = input('Nadaj imie swojemu Tamagotchi \n')
 
     while True:
-        plec = input('Wybierz płeć:  \n Jeśli dziewczynka, wciśnij 2.\n Jeśli chłopiec, wciśnij 1.\n')
+        plec = input('Wybierz plec:  \n Jesli dziewczynka, wcisnij 2.\n Jesli chlopiec, wcisnij 1.\n')
         if plec == '1':
-            a = 'Chłopiec'
+            a = 'Chlopiec'
             break
         elif plec == '2':
             a = 'Dziewczynka'
             break
         else:
-            print('Wybierz prawidłową cyfrę!')
+            print('Wybierz prawidlowa cyfre!')
 
     while True:
-        zwierze = input('Jakie zwierzę Cię interesuje: \n 1.Chomik \n 2.Piesek \n 3.Kotek \n')
+        zwierze = input('Jakie zwierze Cie interesuje: \n 1.Chomik \n 2.Piesek \n 3.Kotek \n')
         if zwierze == '1':
             b = 'Chomik\n'
             break
@@ -83,18 +83,18 @@ if __name__ == '__main__':
             b = 'Kotek\n'
             break
         else:
-            print('Wybierz zwierzę!')
+            print('Wybierz zwierze!')
 
     potworek = Tamagotchi(a, imie, b)
     potworek.powitanie()
     potworek.jaka_plec()
     potworek.wybierz_zwierzatko()
-    print('Pogłaszcz mnie!')
+    print('Poglaszcz mnie!')
 
     while True:
-        czynnosci = input('\n Co chcesz zrobić z %s ? \n 1.Karmienie\n 2.Mycie\n 3.Leczenie\n 4.Zabawa\n 5.Mizianie\n' % (imie))
+        czynnosci = input('\n Co chcesz zrobic z %s ? \n 1.Karmienie\n 2.Mycie\n 3.Leczenie\n 4.Zabawa\n 5.Mizianie\n' % (imie))
         if czynnosci == '1':
-            karmienie = input('Napisz co ma jeść %s\n' % (imie))
+            karmienie = input('Napisz co ma jesc %s\n' % (imie))
             c = potworek.karmienie(karmienie)
 
             if karmienie == 'karma':
@@ -102,15 +102,15 @@ if __name__ == '__main__':
                 print('Mniam!')
 
         elif czynnosci == '2':
-            kapanie = input('Wpisz gdzie ma się umyć\n')
+            kapanie = input('Wpisz gdzie ma sie umyc\n')
             d = potworek.mycie(kapanie)
 
         elif czynnosci == '3':
-            leczenie = input('Wpisz co potrzebuje Twój Tamagotchi, gdy jest chory\n')
+            leczenie = input('Wpisz co potrzebuje Twoj Tamagotchi, gdy jest chory\n')
             e = potworek.leczenie(leczenie)
 
         elif czynnosci == '4':
-            zabawa = input('Napisz w co chcesz się bawić z %s\n' % (imie))
+            zabawa = input('Napisz w co chcesz sie bawic z %s\n' % (imie))
             f = potworek.zabawa(zabawa)
 
         elif czynnosci == '5':
@@ -119,5 +119,6 @@ if __name__ == '__main__':
 
 
         potworek.uaktualnij()
+        # dodaje tu komentarz
 
 
